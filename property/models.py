bd2bf4bd5907e20fc344b711fba2579d0a450927
Isa Ugurchiev <iugurchiev@gmail.com>
2021-09-23 15:04:00 +0300

    Add Complaint and ComplaintAdmin

--- property/models.py
+++ property/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -50,3 +51,11 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался', related_name='complaints')
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='Квартира, на которую пожаловались', related_name='complaints')
+    text = models.TextField(blank=True, verbose_name='Текст жалобы')
+
+
